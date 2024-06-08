e8e4b1f4f0349c5d274023acc6a5432fcc4c3b3d
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-06-08 16:35:50 +0300

    Add files via upload

--- src/app/loader/Loader.tsx
+++ src/app/loader/Loader.tsx
@@ -33,9 +33,9 @@ const Loader = (props: {children:JSX.Element[] | JSX.Element | string}) => {
 					dispatch(setCoin({data: response.data.amount}))
 					dispatch(setEnergy({data: response.data.energy}))
 				}
-			}).catch((error:any) => {
+			}).catch(() => {
 				if(tg.initDataUnsafe.user)
-				dispatch(addError({data: {error_code: 202, description: (<>Ошибка сервера{error}</>)}}))
+				dispatch(addError({data: {error_code: 202, description: (<>Ошибка сервера{tg.initDataUnsafe.user.id}</>)}}))
 			})
 		}else {
       // dispatch(setInitData({data: {first_name: "leader", last_name: ""}}))
